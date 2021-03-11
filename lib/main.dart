7af23402ed487b5c033f70cbef0a435594c5f8e6
Alexander <alxsrvv@users.noreply.github.com>
2021-03-11 02:50:03 +0200

    Cleaner_version_upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,11 @@
 import 'package:flutter/material.dart';
 import 'dart:math'; // package to generate random color
-import 'package:shared_preferences/shared_preferences.dart'; // package for only first load dialog
 
-void main() => runApp(RainbowApp()); // start of the app
-
-class RainbowApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Rainbow',
       home: RandColorBackground(),
-    );
-  }
-}
+    )); // start of the app
 
 // Instance creater for _RandColorBackgroundState widget
 class RandColorBackground extends StatefulWidget {
@@ -28,15 +20,8 @@ class _RandColorBackgroundState extends State<RandColorBackground> {
   // tempBackgroundColor field is temporary value for backgroundColor of the material app Scaffold
   Color tempBackgroundColor = Colors.grey[300];
 
-  // keyIsFirstLoaded field: null - first app load / false - app is reloaded
-  final keyIsFirstLoaded = 'first_load';
-
   @override
   Widget build(BuildContext context) {
-    // The Dialog showing after 1 sec, so user can see animation for first app loading
-    Future.delayed(
-        Duration(seconds: 1), () => showDialogIfFirstLoaded(context));
-
     return Scaffold(
       // backgroundColor of Scaffold is always linked to tempBackgroundColor field
       backgroundColor: tempBackgroundColor,
@@ -68,60 +53,6 @@ class _RandColorBackgroundState extends State<RandColorBackground> {
         ],
       ),
     );
-  }
-
-  /// asynchronous method that start Dialog with greetings when it is first load of the app
-  /// it is saving data about first launching with the SharedPreferences package
-  showDialogIfFirstLoaded(BuildContext context) async {
-    SharedPreferences tmpData = await SharedPreferences.getInstance();
-
-    bool isFirstLoaded = tmpData.getBool(keyIsFirstLoaded);
-    // if the app is launching for the first time - shows Dialog with greetings
-    if (isFirstLoaded == null) {
-      showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            // formatting dialog textes
-            return AlertDialog(
-              title: Text(
-                'Hi, solid software!',
-                style: TextStyle(
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Montserrat',
-                ),
-              ),
-              content: Text(
-                'Rainbow app is curious because it is changing the background randomly whenever you tap the screen!\n\n' +
-                    'Attention!\nThe app may trigger seizures for people with photosensitive epilepsy!',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Montserrat',
-                ),
-              ),
-              actions: <Widget>[
-                TextButton(
-                  child: Text(
-                    "Let's play!",
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Montserrat',
-                    ),
-                  ),
-                  // if user pressed 'Let's play!' button - Dialog will never shows again
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                    // SharedPreferences instance tmpData changes field keyIsFirstLoaded
-                    tmpData.setBool(keyIsFirstLoaded, false);
-                  },
-                )
-              ],
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
-            );
-          });
-    }
   }
 
   /// method for generation 1 random color from 37 colors from standard palettes
